5e1ecad6df1577ce4bfb3aff004a74afe7f52f77
Hoa <hoameomu@gmail.com>
2015-05-26 18:21:03 +0800

    Add Settings for rest server

---
+++ rest/src/main/scala/com/saikocat/meownificent/configuration/Settings.scala
@@ -0,0 +1,21 @@
+package com.saikocat.meownificent.configuration
+
+// import scala.collection.JavaConverters._
+import com.typesafe.config.Config
+import spray.util.SettingsCompanion
+
+case class Settings(
+    interface: String,
+    port: Int,
+    devMode: Boolean) {
+
+  require(interface.nonEmpty, "interface must not be empty")
+  require(0 < port && port < 65536, "illegal port")
+}
+
+object Settings extends SettingsCompanion[Settings]("meownificent.rest") {
+  def fromSubConfig(config: Config) = apply(
+    config getString "interface",
+    config getInt "port",
+    config getBoolean "dev-mode")
+}
