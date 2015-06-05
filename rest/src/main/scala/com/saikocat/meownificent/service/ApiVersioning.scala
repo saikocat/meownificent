a85ca029b2c3299652e5f0af4b0517e8c26d6799
Hoa <hoameomu@gmail.com>
2015-06-05 12:13:23 +0800

    Remove ApiVersioning via configuration file

--- rest/src/main/scala/com/saikocat/meownificent/service/ApiVersioning.scala
+++
@@ -1,6 +0,0 @@
-package com.saikocat.meownificent.service
-
-trait ApiVersioning {
-  val version: Int
-  lazy val versionMediaType: String = s"application/vnd.meownificent.v${version}+json"
-}
--- rest/src/main/scala/com/saikocat/meownificent/service/RootService.scala
+++ rest/src/main/scala/com/saikocat/meownificent/service/RootService.scala
@@ -9,14 +9,12 @@ import com.saikocat.meownificent.directive.AcceptHeaderDirective
 import com.saikocat.meownificent.error.ApiRejectionHandler
 
 class RootServiceActor(settings: Settings) extends Actor with RootService {
-  val version: Int = settings.version
-
   def actorRefFactory: ActorRefFactory = context
   def receive = runRoute(rootRoute)
 }
 
 trait RootService extends HttpService
-  with ApiVersioning with AcceptHeaderDirective
+  with AcceptHeaderDirective
   with ApiRejectionHandler
   with RedditService {
 
